b5a3d8b9d1c29e810b71b3974f06c10b5e58c8dd
aitchhan <aitchhan@gmail.com>
2025-09-09 14:26:23 +0900

    [add] Setting up...

---
+++ src/main/java/com/sku_sku/clac/ClacApplication.java
@@ -0,0 +1,13 @@
+package com.sku_sku.clac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sku_sku/clac/ClacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sku_sku.clac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
